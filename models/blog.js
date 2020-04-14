0eaaef34cb72291735f20a60ce75b3de0f85b624
Edwin Chow <echow6@gmail.com>
2020-04-13 20:37:56 -0500

    Initial commit

---
+++ models/blog.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: 1,
+        required: true
+    },
+    author: {
+        type: String,
+        minlength: 1,
+        required: true
+    },
+    url: {
+        type: String,
+        minlength: 1,
+        required: true
+    },
+    likes: {
+        type: Number,
+        minlength: 1,
+        required: false
+    },
+});
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
